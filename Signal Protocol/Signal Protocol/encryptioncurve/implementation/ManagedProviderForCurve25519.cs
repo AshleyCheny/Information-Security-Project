62a5c6f8f73a217bfc9bd5c2223932e518e099db
Gaurav Palande <gaurav.palande147@gmail.com>
2016-10-26 16:13:04 -0700

    Included ManagedProviderForCurve25519 for implementing curve25519 for library

---
+++ Signal Protocol/Signal Protocol/encryptioncurve/implementation/ManagedProviderForCurve25519.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signal_Protocol.encryptioncurve.implementation
+{
+    class ManagedProviderForCurve25519 : IProviderCurve25519
+    {
+        private org.whispersystems.curve25519.Curve25519 curve;
+
+        public Curve25519ManagedProvider(string type)
+        {
+            curve = org.whispersystems.curve25519.Curve25519.getInstance(type);
+        }
+
+        public byte[] calculateAgreement(byte[] ourPrivate, byte[] theirPublic)
+        {
+            return curve.calculateAgreement(ourPrivate, theirPublic);
+        }
+
+        public byte[] calculateSignature(byte[] random, byte[] privateKey, byte[] message)
+        {
+            return curve.calculateSignature(random, privateKey, message);
+        }
+
+        public byte[] generatePrivateKey(byte[] random)
+        {
+            return curve.generatePrivateKey(random);
+        }
+
+        public byte[] generatePublicKey(byte[] privateKey)
+        {
+            return curve.generatePublicKey(privateKey);
+        }
+
+        public bool isNative()
+        {
+            return curve.isNative();
+        }
+
+        public bool verifySignature(byte[] publicKey, byte[] message, byte[] signature)
+        {
+            return curve.verifySignature(publicKey, message, signature);
+        }
+    }
+}
