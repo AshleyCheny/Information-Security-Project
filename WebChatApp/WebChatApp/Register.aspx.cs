9d6fefbbcb5318a27e022a9be219042ef33376fe
Ashley Chen <AshleyCheny@gmail.com>
2016-10-31 17:49:54 -0700

    Create a web chat app based on ASP.NET Web Form. Add three web form - Register.aspx, Login.aspx and Chat.aspx

---
+++ WebChatApp/WebChatApp/Register.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//https://www.youtube.com/watch?v=_U5nwI9Fq7o
+namespace WebChatApp
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
