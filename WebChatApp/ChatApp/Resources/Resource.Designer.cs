e5920ca3177ffb28bedbd9b81a3a63993880a3ad
Ashle <Ashle@DESKTOP-546B47R>
2016-11-07 13:39:14 -0800

    Set the android manifest for our ChatApp android project.

--- WebChatApp/ChatApp/Resources/Resource.Designer.cs
+++ WebChatApp/ChatApp/Resources/Resource.Designer.cs
@@ -60,11 +60,14 @@ namespace ChatApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int MyButton = 2131034113;
+			// aapt resource value: 0x7f050002
+			public const int MyButton = 2131034114;
 			
 			// aapt resource value: 0x7f050000
-			public const int myText = 2131034112;
+			public const int finish = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int myText = 2131034113;
 			
 			static Id()
 			{
@@ -80,7 +83,10 @@ namespace ChatApp
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
