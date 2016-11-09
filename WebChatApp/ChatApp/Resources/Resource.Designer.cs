2f4fe7b3b5cf0885feec4ad9042f4d2929935d44
Ashle <Ashle@DESKTOP-546B47R>
2016-11-09 13:44:58 -0800

    Add two layouts which will be used in the Conversations screen later on.

--- WebChatApp/ChatApp/Resources/Resource.Designer.cs
+++ WebChatApp/ChatApp/Resources/Resource.Designer.cs
@@ -60,38 +60,47 @@ namespace ChatApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int login = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int messageList = 2131034115;
-			
-			// aapt resource value: 0x7f050005
-			public const int messageText = 2131034117;
-			
-			// aapt resource value: 0x7f050008
-			public const int myMessageDate = 2131034120;
-			
-			// aapt resource value: 0x7f050007
-			public const int myMessageText = 2131034119;
-			
 			// aapt resource value: 0x7f050001
-			public const int password = 2131034113;
-			
-			// aapt resource value: 0x7f050004
-			public const int relativeLayout1 = 2131034116;
-			
-			// aapt resource value: 0x7f050006
-			public const int sendButton = 2131034118;
-			
-			// aapt resource value: 0x7f05000a
-			public const int theirMessageDate = 2131034122;
-			
-			// aapt resource value: 0x7f050009
-			public const int theirMessageText = 2131034121;
+			public const int conversationLastMessage = 2131034113;
 			
 			// aapt resource value: 0x7f050000
-			public const int username = 2131034112;
+			public const int conversationUsername = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int conversationsList = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int login = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int messageList = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int messageText = 2131034120;
+			
+			// aapt resource value: 0x7f05000b
+			public const int myMessageDate = 2131034123;
+			
+			// aapt resource value: 0x7f05000a
+			public const int myMessageText = 2131034122;
+			
+			// aapt resource value: 0x7f050004
+			public const int password = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int relativeLayout1 = 2131034119;
+			
+			// aapt resource value: 0x7f050009
+			public const int sendButton = 2131034121;
+			
+			// aapt resource value: 0x7f05000d
+			public const int theirMessageDate = 2131034125;
+			
+			// aapt resource value: 0x7f05000c
+			public const int theirMessageText = 2131034124;
+			
+			// aapt resource value: 0x7f050003
+			public const int username = 2131034115;
 			
 			static Id()
 			{
@@ -107,16 +116,22 @@ namespace ChatApp
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Login = 2130903040;
+			public const int ConversationListItem = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Messages = 2130903041;
+			public const int Conversations = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int MyMessageListItem = 2130903042;
+			public const int Login = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int TheirMessageListItem = 2130903043;
+			public const int Messages = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int MyMessageListItem = 2130903044;
+			
+			// aapt resource value: 0x7f030005
+			public const int TheirMessageListItem = 2130903045;
 			
 			static Layout()
 			{
